6a7888e54dc08518a6f1d6f1ce95cebd890aaf6e
dave <dave@aurora.(none)>
2011-02-24 18:12:22 +0000

    Adding in the glorious "subject" method.

--- rails_generators/shoulda_machinist_model/templates/unit_test.rb
+++ rails_generators/shoulda_machinist_model/templates/unit_test.rb
@@ -1,8 +1,15 @@
-require 'test_helper'
+  require 'test_helper'
 
 class <%= class_name %>Test < ActiveSupport::TestCase
 
   context "The #{class_name} model" do
+
+  	setup do
+  		@<%= file_name %> = <%= class_name %>.make
+  	end
+
+    subject { @<%= file_name %> }
+
   <% for attribute in attributes -%>
     should_have_db_column :<%= attribute.name %>
   <% end -%>
